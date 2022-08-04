c96049ef0f76f28910433ce8af4ec6d740638557
Raniya Abdalla <58631480+raniyaabdalla@users.noreply.github.com>
2022-08-04 00:10:28 -0400

    Done

--- script.js
+++ script.js
@@ -4,12 +4,34 @@ window.addEventListener("load", function() {
 
    let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
+   let listedPlanetsResponse = myFetch();
    listedPlanetsResponse.then(function (result) {
        listedPlanets = result;
        console.log(listedPlanets);
-   }).then(function () {
-       console.log(listedPlanets);
+   
+   })
+       .then(function () {
+        console.log(listedPlanets);
+        let planetSelection = pickPlanet(listedPlanets);
+        addDestinationInfo(
+          document,
+          planetSelection.name,
+          planetSelection.diameter,
+          planetSelection.star,
+          planetSelection.distance,
+          planetSelection.moons,
+          planetSelection.image
+        );
+        let pilot = document.querySelector("input[name=pilotName]");
+        let copilot = document.querySelector("input[name=copilotName]");
+        let fuelLevel = document.querySelector("input[name=fuelLevel]");
+        let cargoLevel = document.querySelector("input[name=cargoMass]");
+        let list = document.getElementById("faultyItems");
+  
+        formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel);
+  
+
+
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
    })
    
